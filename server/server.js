7bb2e043eb9373615d0fc7281646bf46012d8863
Ahmedmohammed1323 <ahmednomann.31@gmail.com>
2025-12-17 11:38:18 +0800

    Add Surge domain to CORS allowed origins

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ app.use(helmet());
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:3001'],
+  origin: ['http://localhost:3000', 'http://localhost:3001', 'https://cybergaurd-academy.surge.sh'],
   credentials: true
 }));
 app.use(express.json());
